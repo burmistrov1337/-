4653efe80d162be53e9a494f172c644c1b81a9f1
burmistrov1337 <burmistrov.ma@mail.ru>
2022-09-03 15:03:22 +0700

    a

--- Вебинар №9/Program.cs
+++ Вебинар №9/Program.cs
@@ -1,1 +1,33 @@
-﻿
+﻿Console.Write("Введите натуральное число N: ");
+string N = Console.ReadLine();
+
+var count = ExceptionHandling(n);
+
+if (count.exception && count.number >= 0)
+{
+    Numbers(count.number);
+}
+else
+{
+    Console.WriteLine($"{N} -> Не является натуральным числом.");
+}
+
+void Numbers(int last)
+{
+    if (last == 0)
+    {
+        return;
+    }
+    Console.Write($"{last} ");
+    Numbers(last - 1);
+}
+
+(bool exception, int number) ExceptionHandling(string number)
+{
+    bool yesInt = int.TryParse(number, out int d);
+    if (!yesInt)
+    {
+        return (false, d);
+    }
+    return (true, d);
+}
