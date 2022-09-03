653d39ebea32a86c7197339772d29f5229db38af
burmistrov1337 <burmistrov.ma@mail.ru>
2022-09-03 18:06:43 +0700

    ex5

---
+++ Example005_ElseIf/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "max")
+
+{
+    Console.Write("U are welcome, ");
+    Console.WriteLine(username);
+}
+
+else
+
+{
+Console.Write("U are not my Daddy, ");
+Console.WriteLine(username);
+}
