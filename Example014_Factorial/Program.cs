92cfe1637e364b65b4695a3e0fd92f7fcd266252
burmistrov1337 <burmistrov.ma@mail.ru>
2022-09-06 21:55:19 +0700

    ex014

--- Example014_Factorial/Program.cs
+++ Example014_Factorial/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// №1 Factorial
+// double Factorial(int n)
+// {
+//     // 1! = 1
+//     // 0! = 1
+//     if (n == 1) return 1;
+//     else return n * Factorial(n-1);
+// }
+// for (int i = 1; i < 40; i++)
+// {
+//     Console.WriteLine($"{i}! = {Factorial(i)}");
+// Console.WriteLine(); 
+// }
+// 
+
+// №2 Fibonacci
+
+// f(1) = 1
+// f(2) = 1
+// f (n) = f(n-1) + f(n-2)
+
+double Fibonacci(int n)
+{
+    if (n == 1 || n == 2) return 1;
+    else return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+for (int i = 1; i < 50; i++)
+{
+    Console.WriteLine($"f({i})= {Fibonacci(i)}");
+}
